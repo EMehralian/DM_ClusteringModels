1a6d61cf6b2a4f60b62272cd8959cfa6075b11d1
Ehsan <ehsan.mehralian@gmail.com>
2017-11-21 20:45:30 +0330

    change comments

--- Q1/OutlierDetectionUnsupervised.py
+++ Q1/OutlierDetectionUnsupervised.py
@@ -22,32 +22,32 @@ yTrain = pd.read_csv("cardioY.csv")
 # print(xTrain.head())
 e = EmpiricalCovariance().fit(xTrain)
 
-# print(__doc__)
-#
-# rng = np.random.RandomState(42)
-#
-# # define two outlier detection tools to be compared
-# classifiers = {
-#     "One-Class SVM": svm.OneClassSVM(nu=0.95 * 0.25 + 0.05,
-#                                      kernel="rbf", gamma=0.1),
-#     "Isolation Forest": IsolationForest(max_samples=80,
-#                                         contamination=0.25,
-#                                         random_state=rng)
-# }
-#
-# # Fit the problem with varying cluster separation
-# X = xTrain
-# print(X.shape)
-#
-# # Fit the model
-# for i, (clf_name, clf) in enumerate(classifiers.items()):
-#     clf.fit(X)
-#     y_pred = clf.predict(X)
-#     print(y_pred)
-#
-# t = 0
-# for i in range(0, len(y_pred)):
-#     if (y_pred[i] == 1):
-#         t = t + 1
-#
-# print(t)
+print(__doc__)
+
+rng = np.random.RandomState(42)
+
+# define two outlier detection tools to be compared
+classifiers = {
+    "One-Class SVM": svm.OneClassSVM(nu=0.95 * 0.25 + 0.05,
+                                     kernel="rbf", gamma=0.1),
+    "Isolation Forest": IsolationForest(max_samples=80,
+                                        contamination=0.25,
+                                        random_state=rng)
+}
+
+# Fit the problem with varying cluster separation
+X = xTrain
+print(X.shape)
+
+# Fit the model
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    clf.fit(X)
+    y_pred = clf.predict(X)
+    print(y_pred)
+
+t = 0
+for i in range(0, len(y_pred)):
+    if (y_pred[i] == 1):
+        t = t + 1
+
+print(t)
